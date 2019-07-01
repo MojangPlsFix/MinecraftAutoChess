9b498cde4b0e9d19eb1810e05367e80a53c19ce9
MojangPlsFix <fabrossmann@gmail.com>
2019-07-01 21:47:45 +0200

    Initial Commit

---
+++ src/main/java/com/fabianrossmann/minecraftautochess/MinecraftAutoChess.java
@@ -0,0 +1,15 @@
+package com.fabianrossmann.minecraftautochess;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftAutoChess extends JavaPlugin {
+
+    @Override public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
